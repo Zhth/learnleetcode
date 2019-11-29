3d3b784ca89d20eaf5dc349a807567db92719695
unknown <328404526@qq.com>
2019-11-29 19:31:39 +0800

    19/11/29

---
+++ 145.二叉树的后序遍历.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode.cn id=145 lang=python3
+#
+# [145] 二叉树的后序遍历
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if root is None:
+            return res
+        stack = [root]
+        lastvisited = root
+        while stack:
+            tempNode = stack.pop()
+            if lastvisited == tempNode.right or lastvisited == tempNode.left:
+                res.append(tempNode.val)
+                lastvisited = tempNode
+            elif tempNode.left is None and tempNode.right is None:
+                res.append(tempNode.val)
+                lastvisited = tempNode
+            else:
+                stack.append(tempNode)
+                if tempNode.right:
+                    stack.append(tempNode.right)
+                if tempNode.left:
+                    stack.append(tempNode.left)
+        return res
+    #     res = []
+    #     self.postorderCur(res, root)
+    #     return res
+    # def postorderCur(self, res, root):
+    #     if root is None:
+    #         return
+    #     self.postorderCur(res, root.left)
+    #     self.postorderCur(res, root.right)
+    #     res.append(root.val)
+    #     return
+
+
+# @lc code=end
---
+++ guessNumber.py
@@ -0,0 +1,63 @@
+import random
+import os
+import time
+
+N = 4
+playPerson = 2
+
+def getNums():
+    nums = []
+    for _ in range(N):
+        t = str(random.randint(0, 9))
+        while t in nums:
+            t = str(random.randint(0, 9))
+        nums.append(t)
+    return nums
+
+
+def play(nums):
+    contStep = 0
+    flag = True
+    while flag:
+        contStep += 1
+        temp = list(input('Please gauss the number (0000 ~ 9999):'))
+        contA, contB = 0, 0
+        for ind in range(N):
+            if temp[ind] == nums[ind]:
+                contA += 1
+            elif temp[ind] in nums:
+                contB += 1
+        print('Your anser :' + str(contA) + 'A' + str(contB) + 'B')
+        if contA == 4:
+            print('You win!')
+            print('You use ' + str(contStep) + ' steps.')
+            flag = False
+        else:
+            print('Try again!')
+    return contStep
+
+
+def writeScore(filename, numOfperson, score, playtime):
+    numOfperson += 1
+    with open(filename, 'a+') as f:
+        f.write('the score of Person '+str(numOfperson)+' is : '+str(score)+'\n')
+        f.write('the time of Person '+str(numOfperson)+' is : '+str(playtime)+'\n')
+    return
+
+
+def clearFile(filename):
+    with open(filename, 'w'):
+        pass
+
+if __name__ == "__main__":
+    nums = getNums()
+    filename = 'score.txt'
+    clearFile(filename)
+    for num in range(playPerson):
+        starttime = time.time()
+        score = play(nums)
+        endtime = time.time()
+        playtime = str(endtime - starttime)
+        writeScore(filename, num, score, playtime)
+        input('Please put enter before next person play.')
+        os.system('cls')
